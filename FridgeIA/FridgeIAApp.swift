f5fb474aa4f886ede6cb1f469c59af1251b0c8f6
Brandon Luis Chavez Choque <brandon.chavez.c@tecsup.edu.pe>
2026-07-02 18:33:16 -0500

    Initial Commit

---
+++ FridgeIA/FridgeIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeIAApp.swift
+//  FridgeIA
+//
+//  Created by BRANDON CHAVEZ on 2/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FridgeIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FridgeIATests/FridgeIATests.swift
@@ -0,0 +1,19 @@
+//
+//  FridgeIATests.swift
+//  FridgeIATests
+//
+//  Created by BRANDON CHAVEZ on 2/07/26.
+//
+
+import Testing
+@testable import FridgeIA
+
+struct FridgeIATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
